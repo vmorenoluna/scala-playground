43dc4815b3095ab4ffc7fc05e3293af1a1a019df
vmorenoluna <morenolunav@gmail.com>
2020-04-04 13:03:33 +0100

    Added example for pattern matching on LazyList

--- src/main/scala/basics/LazyList.scala
+++ src/main/scala/basics/LazyList.scala
@@ -6,4 +6,16 @@ object LazyList extends App {
 
   naturals take 10 foreach println
 
+  println("\u2500" * 50)
+
+  def getAt[A](s: LazyList[A], i: Int): A = (s, i) match {
+    case (h #:: _, 0) => h
+    case (_ #:: t, _) => getAt(t, i - 1)
+    case _ => throw new NoSuchElementException("getAt(empty)")
+  }
+
+  println(
+    getAt[Int](naturals take 10, 5)
+  )
+
 }
